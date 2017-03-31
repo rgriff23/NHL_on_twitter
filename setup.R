f6456f11047df4e618c22ec26696fd743606a582
rgriff23 <rgriff23@gmail.com>
2017-03-31 09:30:58 +0900

    improve figs

--- setup.R
+++ setup.R
@@ -54,11 +54,28 @@ uswnt.followers <- sapply(uswnt.data, function(i) i$followersCount)
 nhl.data <- lookupUsers(nhl.screennames)
 nhl.followers <- sapply(nhl.data, function(i) i$followersCount)
 
+# identify as many NHL teams as possible using descriptions
+
+# manually fill in blanks for NHL teams
+
 ######################
 # VISUALIZE RESULTS #
 ####################
 
 # histogram with top USWNT players depicted
+layout(matrix(1:2,1,2))
+hist(nhl.followers/1000, breaks=100, col="gray", main="NHL", xlab="Number of Followers (thousands)")
+hist(uswnt.followers/1000, breaks=10, col="gray", main="USWNT", ylab="", xlab="Number of Followers (thousands)")
+
+# log histogram with top USWNT players depicted
+hist(log(nhl.followers), 
+     breaks=100, 
+     col="gray", 
+     xlim=log(c(min(uswnt.followers), max(nhl.followers))),
+     xlab="Log (Number of Followers)",
+     main="")
+abline(v=log(uswnt.followers), col="red")
+abline(v=mean(log(nhl.followers)), col="blue", lwd=3)
 
 ########
 # END #
