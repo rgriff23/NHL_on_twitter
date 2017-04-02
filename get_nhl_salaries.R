0979af058f53fc8ef15f11948a0bf7058e0bf34f
rgriff23 <rgriff23@gmail.com>
2017-04-02 15:18:55 +0900

    rm commas frm numbers

--- get_nhl_salaries.R
+++ get_nhl_salaries.R
@@ -6,5 +6,8 @@ library("rvest")
 salaries <- read_html("http://www.hockey-reference.com/friv/current_nhl_salaries.cgi")
 salaries <- data.frame(html_table(html_node(salaries, 'table')))
 
+# get rid of commas in numbers and convert to numeric
+salaries$Salary <- as.numeric(gsub(",","",salaries$Salary))
+
 # export table
 write.csv(salaries, file="~/Desktop/GitHub/NHL_on_twitter/data/nhl_salaries.csv", row.names=F)
