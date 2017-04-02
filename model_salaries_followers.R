cd6d6e6acb1d72bc6bcd50811ab7708901c8f5ba
rgriff23 <rgriff23@gmail.com>
2017-04-02 17:01:51 +0900

    add uswnt prediction

--- model_salaries_followers.R
+++ model_salaries_followers.R
@@ -3,3 +3,11 @@ fit <- nls(Salary/1000000 ~ SSlogis(log.Followers, Asym, xmid, scal), data = nhl
 y=predict(fit, list(log.Followers=x))
 plot(Salary/1e+06 ~ log.Followers, data = nhl.followers.salaries.clean)
 lines(x,y)
+
+# use model to make salary preditions for USWNT
+uswnt.followers$log.Followers <- log(uswnt.followers$Followers)
+predictions <- predict(fit, list(log.Followers=uswnt.followers$log.Followers))
+uswnt.followers$PredictedSalary <- predictions*1000000
+
+# sort USWNT data
+uswnt.followers[sort.int(uswnt.followers$PredictedSalary, index.return=T, decreasing=T)$ix,]
