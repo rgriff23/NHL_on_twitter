1e2f56f8ddedc9cc5563e6f18e379a4474fb90c1
rgriff23 <rgriff23@gmail.com>
2017-04-04 01:05:07 +0900

    rm hist

--- get_follower_counts.R
+++ get_follower_counts.R
@@ -2,9 +2,9 @@
 # twitter authentication
 source('~/Dropbox/Code/R/twitter_setup.R', chdir = TRUE)
 
-###################
-# GET PLAYER IDS #
-#################
+################################################
+# GET PLAYER IDS FROM PUBLISHED TWITTER LISTS #
+##############################################
 
 # get USWNT players
 uswnt.api.url <- paste0("https://api.twitter.com/1.1/lists/members.json?slug=",
@@ -47,27 +47,6 @@ write.csv(uswnt.data, file="~/Desktop/GitHub/NHL_on_twitter/data/uswnt_follower_
 nhl.data <- data.frame(Player=nhl.names, Handle=nhl.screennames, Followers=nhl.followers)
 write.csv(nhl.data, file="~/Desktop/GitHub/NHL_on_twitter/data/nhl_follower_counts.csv", row.names=F)
 
-###################
-# VISUALIZE DATA #
-#################
-
-# histogram with top USWNT players depicted
-layout(matrix(1:2,1,2))
-hist(nhl.followers/1000, breaks=100, col="gray", main="NHL", xlab="Number of Followers (thousands)")
-hist(uswnt.followers/1000, breaks=10, col="gray", main="USWNT", ylab="", xlab="Number of Followers (thousands)")
-
-# log histogram with top USWNT players depicted
-hist(log(nhl.followers), 
-     breaks=100, 
-     col="gray", 
-     xlim=log(c(min(uswnt.followers), max(nhl.followers))),
-     xlab="Log (Number of Followers)",
-     main="")
-abline(v=log(uswnt.followers), col="red")
-abline(v=mean(log(nhl.followers)), col="blue", lwd=3)
-
 ########
 # END #
 ######
-
-
